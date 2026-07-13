120e7e75de342a4ddf34b3a4517b0a9e1ce04869
alesia870-web <alesianarcis870@gmail.com>
2026-07-13 07:48:58 +0300

    Update service worker cache and add manifest support

--- sw.js
+++ sw.js
@@ -1,10 +1,46 @@
-var CACHE = 'spin-kingdom-v1';
-var URLS = ['./', './index.html'];
+var CACHE = 'spin-kingdom-v2';
+var URLS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(caches.open(CACHE).then(function(cache) { return cache.addAll(URLS); }));
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(function (cache) {
+        return cache.addAll(URLS);
+      })
+      .then(function () {
+        return self.skipWaiting();
+      })
+  );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(caches.match(e.request).then(function(r) { return r || fetch(e.request); }));
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.map(function (key) {
+          if (key !== CACHE) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(function () {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') return;
+
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      return r || fetch(e.request);
+    })
+  );
 });
